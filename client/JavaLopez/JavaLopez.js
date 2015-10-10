625004b63611ea554834167109fe1b8b6dce33cc
Duran Cristhian <durancristhian@gmail.com>
2015-10-10 16:34:34 -0300

    JavaLopez view

---
+++ client/JavaLopez/JavaLopez.js
@@ -0,0 +1,11 @@
+Template.JavaLopez.helpers({
+  column: function (argument) {
+  	// return [
+	  //   { name: "Column 1" },
+	  //   { name: "Column 2" },
+	  //   { name: "Column 3" }
+	  // ];
+
+	  return Game
+  }
+});
